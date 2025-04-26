0c7110e2b1877d8bdfd4d55a4710a82318056082
Rafael Gutkowski <nounderline@libred.org>
2025-04-26 12:43:37 +0200

    Add Map

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -13,6 +13,14 @@ export default function Home() {
       <div class="md:h-8">
       </div>
 
+      <div class="flex flex-col w-full max-w-[1140px] h-195 mx-auto z-10 relative gap-8 overflow-hidden rounded-lg shadow-sm">
+        <iframe
+          src="https://homebase-map-nextjs.vercel.app"
+          style="width: 100%; height: 100%; margin: 0; padding: 0; border: none;"
+          allow="geolocation"
+        />
+      </div>
+
       <div class="flex flex-col pt-6 w-full max-w-[840px] mx-auto z-10 relative gap-8 sm:px-4">
         <WorkshopListCard />
       </div>

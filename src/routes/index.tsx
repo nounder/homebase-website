b90ca0395608ff70a4566146490047071e53f8b0
Rafael Gutkowski <nounderline@libred.org>
2025-05-01 15:31:25 -0400

    Update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,16 @@
 import { Title } from "@solidjs/meta"
-import { For } from "solid-js"
+import { For, onMount } from "solid-js"
 import { BasedHouseCard } from "../ui/BasedHouse"
 import { Header } from "../ui/Header"
 import { VideoGallery } from "../ui/VideoGallery"
 import { WorkshopListCard } from "../ui/Workshop"
 
 export default function Home() {
+  onMount(() => {
+    import("@farcaster/frame-sdk")
+      .then(mod => mod.sdk.actions.ready())
+  })
+
   return (
     <main>
       <Header />
@@ -16,8 +21,9 @@ export default function Home() {
       <div class="flex flex-col w-full max-w-[1140px] h-195 mx-auto z-10 relative gap-8 overflow-hidden rounded-lg shadow-sm">
         <iframe
           src="https://homebase-map-nextjs.vercel.app"
-          style="width: 100%; height: 100%; margin: 0; padding: 0; border: none;"
+          style="width: 100%; height: 100%; margin: 0; padding: 0; border: none; overflow: hidden;"
           allow="geolocation"
+          scrolling="no"
         />
       </div>
 

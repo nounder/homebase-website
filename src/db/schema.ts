ded5274f6bdc89c743631862a2be8efe13d4c2c8
Rafael Gutkowski <nounderline@libred.org>
2025-07-09 17:24:07 -0400

    Effect.fn-like API for sqlSchema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,20 @@
-import { SqlClient, SqlError, SqlResolver, SqlSchema } from "@effect/sql"
-import { Effect, Schema } from "effect"
+import { SqlClient, SqlError } from "@effect/sql"
+import { Console, Effect, Schema } from "effect"
+import { YieldWrap } from "effect/Utils"
+
+type NonGenMapper<IA> = (
+  req: IA,
+  sql: SqlClient.SqlClient,
+) => Effect.Effect<unknown, SqlError.SqlError>
+
+type GenMapper<IA> = (
+  req: IA,
+  sql: SqlClient.SqlClient,
+) => Generator<YieldWrap<Effect.Effect<unknown, SqlError.SqlError>>>
+
+type Mapper<IA> =
+  | NonGenMapper<IA>
+  | GenMapper<IA>
 
 export function sqlSchema<
   II,
@@ -11,24 +26,24 @@ export function sqlSchema<
   Result: Schema.Schema<AA, AI, never>,
 ) {
   return (
-    map: (
-      req: IA,
-      sql: SqlClient.SqlClient,
-    ) => Effect.Effect<unknown, SqlError.SqlError>,
+    map: Mapper<IA>,
   ) => {
     const decodeRequest = Schema.decode(Request)
     const decodeResult = Schema.decodeUnknown(Result)
+    // we have to `as any` probably because of a bug:
+    // @see https://github.com/Effect-TS/effect/issues/5190
+    const mapFn = Effect.fn(map as any) as NonGenMapper<IA>
 
     return Object.assign(
-      (req: II) =>
-        Effect.gen(function*() {
-          const sql = yield* SqlClient.SqlClient
-          const validatedReq = yield* decodeRequest(req)
-          const rows = yield* map(validatedReq, sql)
-          const result = yield* decodeResult(rows)
+      Effect.fnUntraced(function*(req: II) {
+        const sql = yield* SqlClient.SqlClient
+        const validatedReq = yield* decodeRequest(req)
+        const rows = yield* mapFn(validatedReq, sql)
 
-          return result as AA
-        }),
+        const result = yield* decodeResult(rows)
+
+        return result as AA
+      }),
       {
         Request,
         Result,
